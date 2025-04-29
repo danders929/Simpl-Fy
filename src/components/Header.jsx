589fda64a95aa655698103e2d2aa861094dbeda6
danders929 <sandersda@gmail.com>
2025-04-29 12:12:18 -0500

    added code for header componenent. Added Code for header.css. Removed old vite logos.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import './Header.css'
-import logo from '../assets/simpl-fy-logo.png'
+import logo from '../../public/assets/simpl-fy_logo_Transparent.png'
 
 export default function Header() {
   return (
     <header className="header">
       <div className="logo-container">
         <img src={logo} alt="Simpl-FY Logo" className="logo" />
-        <h1>Simpl-FY</h1>
+        
       </div>
       <nav className="nav-links">
         <a href="#services">Services</a>
