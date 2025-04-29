618bb453055caa66b5c9094bd8ba746179890e32
danders929 <sandersda@gmail.com>
2025-04-29 12:32:13 -0500

    restart

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import Header from './components/Header'
 import './App.css'
 
 function App() {
   return (
     <>
-      <Header />
-
+      <header />
       <main>
-        <section id="services">IT Support and Modern Solutions</section>
-        <section id="about">About Simpl-FY</section>
-        <section id="contact">Contact Us</section>
       </main>
-
       <footer>
-        <p>&copy; 2025 Simpl-FY LLC</p>
       </footer>
     </>
   )
